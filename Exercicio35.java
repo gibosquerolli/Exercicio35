b59554c51361aaa98998d273870d8bcbb6bf5ad6
Giovanna Bosquerolli <1bosquerolligiovanna@gmail.com>
2025-04-30 11:33:32 -0300

    Add files via upload

---
+++ Exercicio35.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exercicio19.exercicio35;
+
+/**
+ *
+ * @author camil
+ */
+public class Exercicio35 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
